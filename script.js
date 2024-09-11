7da5fc7b64d5becf0be91c49704135c74cf1351e
Nguyễn Main Đan Trường <nguyenmaidantruongttcspm9866@gmail.com>
2024-09-11 14:32:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const sideNav = document.getElementById('side-nav');
-
-    mobileMenu.addEventListener('click', function () {
-        // Kiểm tra trạng thái của thanh điều hướng
-        if (sideNav.style.width === '250px') {
-            sideNav.style.width = '0';
-        } else {
-            sideNav.style.width = '250px';
-        }
-    });
+document.getElementById("menu-bar").addEventListener("click", function() {
+    this.classList.toggle("active");
+    document.getElementById("side-nav").classList.toggle("active");
 });
